8f63946217b3d8b4be00553569c0562b10ee792a
anuneet1610 <98578428+anuneet1610@users.noreply.github.com>
2026-07-31 22:53:45 +0530

    Update and rename collisions.py to collisions.py

--- Pygame_version/collisions.py
+++ Pygame_version/collisions.py
@@ -1,8 +1,5 @@
-import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 import math
-import Pygame_version.state
+import state
 
 
 # ------------------------
